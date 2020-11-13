e14e3e7a12e38b4d65ec9aa7b70d9ad2de28f4ea
Josh Janikowski <janikows@fvtc.edu>
2020-11-12 20:25:51 -0600

    Driveable Jeep

--- Assets/DriveTest.cs
+++ Assets/DriveTest.cs
@@ -4,25 +4,40 @@ using UnityEngine;
 
 public class DriveTest : MonoBehaviour
 {
-    public WheelCollider WC;
-    public GameObject Wheels;
-
+    public WheelCollider[] WCs;
+    public GameObject[] Wheels;
+    public float maxSteerAngle = 30;
     public float torque = 200;
     // Start is called before the first frame update
     void Start()
     {
-        WC = this.GetComponent<WheelCollider>();
+
     }
-    void Go(float accel )
+    void Go(float accel, float steer)
     {
         accel = Mathf.Clamp(accel, -1, 1);
+        steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
         float thrustTorque = accel * torque;
-        WC.motorTorque = thrustTorque;
+        for (int i = 0; i < 4; i++)
+        {
+            WCs[i].motorTorque = thrustTorque;
+            if(i<2)
+            WCs[i].steerAngle = steer;
+
+            Quaternion quat;
+            Vector3 postion;
+            WCs[i].GetWorldPose(out postion, out quat);
+            Wheels[i].transform.position = postion;
+            Wheels[i].transform.rotation = quat;
+        }
+
+
     }
     // Update is called once per frame
     void Update()
     {
         float a = Input.GetAxis("Vertical");
-        Go(a);
+        float s = Input.GetAxis("Horizontal");
+        Go(a, s);
     }
 }
